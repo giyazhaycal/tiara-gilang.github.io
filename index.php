3c2674fa4811beb99306605e807d1b3d6141c829
haycalgiyaz <48546201+haycalgiyaz@users.noreply.github.com>
2022-05-20 09:27:15 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Wedding Invitation</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/custom-styles.css">
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="main-envelope">
+		<div class="outline-wrappeer">
+			<div class="wrappeer">
+				<h2 class="bride m-2">Tiara & Gilang</h2>
+			</div>
+		</div>
+	</div>
+
+</body>
+
+<script type="text/javascript">
+	// $('')
+</script>
+</html>
